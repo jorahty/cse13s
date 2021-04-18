c0e585ceaa2bb8b8bfdd6f98c4a8bca36f91c767
James Tennant <jtennant@ucsc.edu>
2021-04-17 21:01:00 -0700

    arcSin(x) is now somewhat accurate

--- asgn2/mathlib.c
+++ asgn2/mathlib.c
@@ -3,7 +3,12 @@
 #include "mathlib.h"
 
 double arcSin(double x) {
-    return (x * 42);
+    double n5 = x;
+    n5 = n5 + x * x * x / 6;
+    n5 = n5 + 3 * x * x * x * x * x / 40;
+    n5 = n5 + 5 * x * x * x * x * x * x * x / 16;
+    n5 = n5 + 35 * x * x * x * x * x * x * x * x * x / 1152;
+    return n5;
 }
 
 double arcCos(double x) {
