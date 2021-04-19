849805b4db91d363892b4cf8781d01ac5e9e9712
James Tennant <jtennant@ucsc.edu>
2021-04-18 20:45:19 -0700

    arcSin(x) is now fairly accurate

--- asgn2/mathlib.c
+++ asgn2/mathlib.c
@@ -1,14 +1,52 @@
 #include <stdio.h>
+#include <assert.h>
 
 #include "mathlib.h"
 
+#define EPSILON 0.00000000001
+#define PI 3.14159265359
+
+double Abs(double x) {
+    if (x < 0) { return -x; }
+    else { return x; }
+}
+
+double Sqrt(double x) {
+    double y = 1.0;
+    assert(x >= 0);
+    for (double guess = 0.0; Abs(y - guess) > EPSILON; y = (y + x / y) / 2.0) {
+        guess = y;
+    }
+    return y;
+}
+
 double arcSin(double x) {
-    double n5 = x;
-    n5 = n5 + x * x * x / 6;
-    n5 = n5 + 3 * x * x * x * x * x / 40;
-    n5 = n5 + 5 * x * x * x * x * x * x * x / 16;
-    n5 = n5 + 35 * x * x * x * x * x * x * x * x * x / 1152;
-    return n5;
+    if (x < -0.75) {
+        double k = Sqrt(1 - x * x);
+        double n1 = k;
+        double n2 = k * k * k / 6;
+        double n3 = 3 * k * k * k * k * k / 40;
+        double n4 = 5 * k * k * k * k * k * k * k / 112;
+        double n5 = -PI / 2;
+        double sum = n1 + n2 + n3 + n4 + n5;
+        return sum;
+    } else if (x > 0.75) {
+        double k = Sqrt(1 - x * x);
+        double n1 = PI / 2;
+        double n2 = -k;
+        double n3 = -k * k * k / 6;
+        double n4 = -3 * k * k * k * k * k / 40;
+        double n5 = -5 * k * k * k * k * k * k * k / 112;
+        double sum = n1 + n2 + n3 + n4 + n5;
+        return sum;
+    } else {
+        double n1 = x;
+        double n2 = x * x * x / 6;
+        double n3 = 3 * x * x * x * x * x / 40;
+        double n4 = 5 * x * x * x * x * x * x * x / 112;
+        double sum = n1 + n2 + n3 + n4;
+        return sum;
+    }
 }
 
 double arcCos(double x) {
