2089bf41e9265f66aa472eb1026bef0f6a2a0f5c
James Tennant <jtennant@ucsc.edu>
2021-04-22 20:39:55 -0700

    can now print user guide

--- asgn3/sorting.c
+++ asgn3/sorting.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
 void printGuide() {
-    printf("Hello World!\n");
+    // Print guide
+    printf("Select at least one sort to perform.\n");
+    printf("SYNOPSIS\n");
+    printf("   A collection of comparison-based sorting algorithms.\n\n");
+    printf("USAGE\n");
+    printf("   ./sorting [-habsqQo] [-n length] [-p elements] [-r seed]\n\n");
+    printf("OPTIONS\n");
+    printf("   -h              Display program help and usage.\n");
+    printf("   -a              Enable all sorts.\n");
+    printf("   -b              Enable Bubble Sort.\n");
+    printf("   -s              Enable Shell Sort.\n");
+    printf("   -q              Enable Quick Sort (Stack).\n");
+    printf("   -Q              Enable Quick Sort (Queue).\n");
+    printf("   -n length       Specify number of array elements.\n");
+    printf("   -p elements     Specify number of elements to print.\n");
+    printf("   -r seed         Specify random seed.\n");
+    printf("   -o              Use sorted arrays.\n");
+    // This guide was taken from the example program
 }
 
 int main() {
