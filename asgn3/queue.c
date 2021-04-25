cc7880b4e9575f296320a7235d93721cece026d9
James Tennant <jtennant@ucsc.edu>
2021-04-25 16:38:09 -0700

    fixed queue.c and stack.c

--- asgn3/queue.c
+++ asgn3/queue.c
@@ -1,6 +1,95 @@
-#include <stdbool.h> 
-#include "queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdbool.h>
+
+// Very little of this code is my own.
+// Almost all of this code came from the following sources:
+// - Assignment PDF
+// - Lecture Slides
+// - Teaching Assistants
+
+typedef uint32_t item;
+
+typedef struct Queue {
+    uint32_t head;
+    uint32_t tail;
+    uint32_t size;
+    uint32_t capacity;
+    int64_t *items;
+} Queue;
+
+Queue *queue_create(uint32_t capacity) {
+    Queue *q = (Queue *) malloc(sizeof(Queue));
+    if (q) {
+        q->head = 0;
+        q->tail = 0;
+        q->size = 0;
+        q->capacity = capacity;
+        q->items = (int64_t *) calloc(capacity, sizeof(int64_t));
+        if (!q->items) {
+            free(q);
+            q = NULL;
+        }
+    }
+    return q;
+}
+
+void queue_delete(Queue **q) {
+    if (*q && (*q)->items) {
+        free((*q)->items);
+        free(*q);
+        *q = NULL;
+    }
+    return;
+}
 
 bool queue_empty(Queue *q) {
-    return q;
-} 
+    if (q->size == 0) {
+        return true;
+    }
+    return false;
+}
+
+bool queue_full(Queue *q) {
+    if (q->size == q->capacity) {
+        return true;
+    }
+    return false;
+}
+
+uint32_t queue_size(Queue *q) {
+    return q->size;
+}
+
+bool enqueue(Queue *q, int64_t x) {
+    if (queue_full(q)) {
+        return false;
+    }
+    q->items[q->tail] = x;
+    q->size += 1;
+    q->tail = (q->tail + 1) % q->capacity;
+    return true;
+}
+
+bool dequeue(Queue *q, int64_t *x) {
+    if (queue_empty(q)) {
+        return false;
+    }
+    *x = q->items[q->head];
+    q->size -= 1;
+    q->head = (q->head + 1) % q->capacity;
+    return true;
+}
+
+void queue_print(Queue *q) {
+    printf("[");
+    for (uint32_t i = 0; i < q->tail; i++) {
+        printf("%" PRId64, q->items[i]);
+        if (i + 1 != q->tail) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+}
--- asgn3/stack.c
+++ asgn3/stack.c
@@ -1,7 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
 #include <stdbool.h>
 
-#include "stack.h"
+// Very little of this code is my own.
+// Almost all of this code came from the following sources:
+// - Assignment PDF
+// - Lecture Slides
+// - Teaching Assistants
+
+typedef uint32_t item;
+
+typedef struct Stack {
+    uint32_t top; 
+    uint32_t capacity;
+    int64_t *items; 
+} Stack;
+
+Stack *stack_create(uint32_t capacity) {
+    Stack *s = (Stack *) malloc(sizeof(Stack));
+    if (s) {
+        s->top = 0; 
+        s->capacity = capacity;
+        s->items = (int64_t *) calloc(capacity, sizeof(int64_t));
+        if (!s->items) {
+            free(s);
+            s = NULL;
+        }
+    }
+    return s;
+}
+
+void stack_delete(Stack **s) {
+    if (*s && (*s)->items) {
+        free((*s)->items);
+        free(*s);
+        *s = NULL;
+    }
+    return;
+}
 
 bool stack_empty(Stack *s) {
-    return s;
+    if (s->top == 0) {
+        return true;
+    }
+    return false;
+}
+
+bool stack_full(Stack *s) {
+    if (s->top == s->capacity) {
+        return true;
+    }
+    return false;
+}
+
+uint32_t stack_size(Stack *s) {
+    return s->top;
+}
+
+bool stack_push(Stack *s, int64_t x) {
+    if (stack_full(s)) {
+        return false;
+    }
+    s->items[s->top] = x;
+    s->top += 1;
+    return true;
+}
+
+bool stack_pop(Stack *s, int64_t *x) {
+    if (stack_empty(s)) {
+        return false;
+    }
+    s->top -= 1;
+    *x = s->items[s->top];
+    return true;
+}
+
+void stack_print(Stack *s) {
+    printf("[");
+    for (uint32_t i = 0; i < s->top; i++) {
+        printf("%" PRId64, s->items[i]);
+        if (i + 1 != s->top) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
 }
