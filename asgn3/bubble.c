93c0d71c2cba4434383cd691a6c460cd96493f06
James Tennant <jtennant@ucsc.edu>
2021-04-26 21:47:06 -0700

    improved bubble sort

--- asgn3/bubble.c
+++ asgn3/bubble.c
@@ -2,21 +2,21 @@
 
 #include <stdint.h>
 
+// Heavily based on the python code provided
 void bubble_sort(uint32_t *A, uint32_t n) {
-    // For every round of swapping ...
-    for (uint32_t i = 0; i < n - 1; i++) {
-        // For every pair
-        for (uint32_t j = 0; j < n - i - 1; j++) {
-            // Compare the two elements in the pair
-            comparisons += 1;
-            // If the element is greater than the next element
-            if (A[j] > A[j + 1]) {
-                // Swap the elements!
-                int aux = A[j]; // Move element to auxilary location
-                A[j] = A[j + 1];
-                A[j + 1] = aux;
-                moves += 3; // A swap requires a total of 3 moves
+    bool swapped = true;
+    while (swapped) {
+        swapped = false;
+        for (uint32_t i = 1; i < n; i++) {
+            comparisons += 1; // Record comparison
+            if (A[i] < A[i - 1]) {
+                int aux = A[i];
+                A[i] = A[i - 1];
+                A[i - 1] = aux;
+                moves += 3; // Record moves
+                swapped = true;
             }
         }
+        n -= 1;
     }
 }
