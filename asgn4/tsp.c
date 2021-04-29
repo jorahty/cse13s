acb18cbeabd327b5fafe6b7c571c13454947cd3b
James Tennant <jtennant@ucsc.edu>
2021-04-29 12:48:34 -0700

    can read/write from stdin/stdout/infile/outfile

--- asgn4/tsp.c
+++ asgn4/tsp.c
@@ -4,37 +4,32 @@
 #include "stack.h"
 
 #include <stdio.h>
+#include <stdint.h>
 #include <getopt.h>
 
 #define OPTIONS "hvui:o:"
-#define BLOCK 4096
 
 int main(int argc, char **argv) {
     
     // Parse command-line options
-    bool verbose = false, undirected = false;
-    FILE *map = stdin, *route = stdout;
+    bool undirected = false, verbose = false;
+    FILE *infile = stdin, *outfile = stdout;
     int opt = 0;
     while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
         switch (opt) {
         case 'h': help(); break;
-        case 'v': verbose = true; break;
         case 'u': undirected = true; break;
-        case 'i': /*map = a file with path optarg;*/ break;
-        case 'o': /*route = a file with path optarg;*/ break;
+        case 'v': verbose = true; break;
+        case 'i': infile = fopen(optarg, "r"); break;
+        case 'o': outfile = fopen(optarg, "w"); break;
         default: help(); break;
         }
     }
-
-    printf("verbose: %s\n", verbose ? "true" : "false");
-    printf("undirected: %s\n", undirected ? "true" : "false");
-    // [echo input file to output file]
     
-    // char buffer[BLOCK];
-    
-    // while ((fscanf(buffer, BLOCK, stdin) != -1)) {
-    //    fputs(buffer, stdout);
-    // }
+    uint32_t vertices;
+    while (fscanf(infile, "%d", &vertices) != EOF) {
+        fprintf(outfile, "vertices = %d\n", vertices);    
+    }
     
     return 0;
 }
