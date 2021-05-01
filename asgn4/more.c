cca55bab995ec51d68820514b61d5b2620caa463
James Tennant <jtennant@ucsc.edu>
2021-04-30 20:40:03 -0700

    added pseudocode for dfs

--- asgn4/more.c
+++ asgn4/more.c
@@ -1,9 +1,36 @@
 #include "more.h"
 
-void dfs(
-    Graph *G, uint32_t v, Path *curr, Path *shortest, char *cities[], FILE *outfile, bool verbose) {
-    printf("Find shortest Hamiltonian path\n");
-}
+void dfs(Graph *G, uint32_t v, Path *curr, Path *shortest, char *cities[], FILE *outfile, bool verbose) {/*
+
+
+    if current length > shortest length {
+        stop searching
+        return
+    }
+    if there is nowhere to go {
+        return
+    }
+    if you need one more vertex and you can reach the start vertex {
+        then you have completed a hamiltonian path
+        push the start vertex
+        update shortest path
+        pop the start vertex
+        return
+    }
+    for w to G->vertices {
+        if G has an unvisited edge at (v, w) {
+            push w to curr stack
+            dfs(G, w)
+            pop w from curr stack
+            label w as unvisited
+        }
+    }
+    write v + shortest path to outfile
+    write total number of recursive calls to outfile
+    return
+
+
+*/}
 
 void help(void) {
     // Print help message and exit
