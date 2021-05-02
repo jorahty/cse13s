547a0ee979b725fa6258a70ce0f288f991e973bb
James Tennant <jtennant@ucsc.edu>
2021-05-01 18:28:26 -0700

    implemented all functions in path.c

--- asgn4/path.c
+++ asgn4/path.c
@@ -50,13 +50,37 @@ bool path_push_vertex(Path *p, uint32_t v, Graph *G) {
 }
 
 bool path_pop_vertex(Path *p, uint32_t *v, Graph *G) {
-
+    // Remove the last vertex that was pushed to the path and store it back in v
+    uint32_t lastvertex;
+    bool successful = stack_pop(p->vertices, lastvertex);    
+    if (successful) {
+        // Subtract the distance from the last vertex on the path to *v
+        p->length -= graph_edge_weight(G, lastvertex, *v);
+        *v = thisvertex // Put last vertex back in v
+    } // Else ...
+    // Pop was unsuccessful or path was empty, both of which should never happen, so simply
+    // Return success status of pop
+    return successful
 }
 
-uint32_t path_vertices(Path *p) {return 0;}
+uint32_t path_vertices(Path *p) {
+    return stack_size(p->vertices); // Return number of cities (vertices) on path p
+}
 
-uint32_t path_length(Path *p) {return 0;}
+uint32_t path_length(Path *p) {
+    return p->length; // Return length of path p
+}
 
-void path_copy(Path *dst, Path *src) {;}
+void path_copy(Path *dst, Path *src) {
+    if (src && dst) { // If source and destination paths exist ...
+        // Set destination-path's vertices to source-path's vertices
+        stack_copy(dst->vertices, src->vertices);
+        dst->length = src->length; // Set destination-path's length to source-path's length
+    }
+    return;
+}
 
-void path_print(Path *p, FILE *outfile, char *cities[]) {;}
+void path_print(Path *p, FILE *outfile, char *cities[]) {
+    fprintf(outfile, "Path length: %d\n", p->length); // Print path length to outfile
+    stack_print(p->vertices, outfile, cities); // Print vertices along the path to outfile
+}
