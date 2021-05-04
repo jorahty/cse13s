9cd4cf441906e381d79b6a3a869a12d75ee140c7
James Tennant <jtennant@ucsc.edu>
2021-05-04 14:59:15 -0700

    clang-formatted encode.c

--- asgn5/encode.c
+++ asgn5/encode.c
@@ -8,21 +8,31 @@
 #define OPTIONS "hi:o:"
 
 int main(int argc, char **argv) {
-    
+
     // Parse command-line options
-    FILE *infile = stdin, *outfile = stdout; // infile/outifile is stdin/stdout by default 
+    FILE *infile = stdin, *outfile = stdout; // infile/outifile is stdin/stdout by default
     // For every option given ...
     int opt;
     while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
         switch (opt) {
-        // If 'h' then print help message, close any opened files, and exit 
-        case 'h': help(); fclose(infile); fclose(outfile); exit(1); break; 
+        // If 'h' then print help message, close any opened files, and exit
+        case 'h':
+            help();
+            fclose(infile);
+            fclose(outfile);
+            exit(0);
+            break;
         // If 'i' then update the infile to the file specified by optarg
-        case 'i': infile = fopen(optarg, "r"); break; 
+        case 'i': infile = fopen(optarg, "r"); break;
         // If 'o' then update the outfile to the file specified by optarg
-        case 'o': outfile = fopen(optarg, "w"); break; 
-        // If an unknown option is given then print help message, close any opened files, and exit 
-        default: help(); fclose(infile); fclose(outfile); exit(1); break; 
+        case 'o': outfile = fopen(optarg, "w"); break;
+        // If an unknown option is given then print help message, close any opened files, and exit
+        default:
+            help();
+            fclose(infile);
+            fclose(outfile);
+            exit(1);
+            break;
         }
     }
 
