10cf6f7f6cc041df0e73d7d38c819bc099aa7509
James Tennant <jtennant@ucsc.edu>
2021-05-05 14:04:12 -0700

    can seperate upper and lower nibbles

--- asgn5/encode.c
+++ asgn5/encode.c
@@ -11,21 +11,22 @@ static void help(char *exec) {
     // (This help message was taken from the example program)
     fprintf(stderr,
         "SYNOPSIS\n"
-        "  A error-injection (noise) program.\n"
+        "  A Hamming(8, 4) systematic code generator.\n"
         "\n"
         "USAGE\n"
-        "  %s [-h] [-s seed] [-e error_rate]\n"
+        "  %s [-h] [-i infile] [-o outfile]\n"
         "\n"
         "OPTIONS\n"
-        "  -h               Program usage and help.\n"
-        "  -s seed          Specifies the random seed.\n"
-        "  -e error_rate    Specifies the error rate.\n",
-        exec); // `exec` is the name of the executable 
+        "  -h             Program usage and help.\n"
+        "  -i infile      Input data to encode.\n"
+        "  -o outfile     Output of encoded data.\n",
+        exec); // `exec` is the name of the execution command
 }
 
 int main(int argc, char **argv) {
 
-    FILE *infile = stdin, *outfile = stdout; // The infile/outfile is stdin/stdout by default
+    // The infile/outfile is stdin/stdout by default
+    FILE *infile = stdin, *outfile = stdout;
 
     // Parse command-line arguments
     int opt;
@@ -46,14 +47,31 @@ int main(int argc, char **argv) {
             help(argv[0]);
             fclose(infile);
             fclose(outfile);
-            return 1; // Return 1 becuase the unknown option has caused an error
+            return 1; // Return 1 becuase the invalid option has caused an error
         }
     }
     
+    // Create the generator matrix used for generating Hamming(8, 4) codes
+    // BitMatrix *G = bm_create(4, 8);
+
+    // Encode every character of the infile and write each Hamming(8, 4) code to the outfile 
     int c; 
-    while ((c = fgetc(infile)) != EOF) {
-        fputc(c, outfile);
+    while ((c = fgetc(infile)) != EOF) { // For every character in the infile ...
+        uint8_t b = (uint8_t) c;
+        uint8_t lower = b & 0x0f;
+        uint8_t upper = (b & 0xf0) >> 4;
+        printf("character code: %d, lower nibble: %d, upper nibble: %d\n", b, lower, upper);
+        // fputc(ham_encode(G, lower), outfile);
+        // fputc(ham_encode(G, upper), outfile);
     }
 
+    // Free the memory allocated for G
+    // bm_delete(&G);
+
+    // Close any opened files
+    fclose(infile);
+    fclose(outfile);
+
+    // Program finished so exit
     return 0;
 }
