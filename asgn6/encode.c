441903d5c0a6bf1c6aa6efe58af5ca29aafeb6f1
James Tennant <jtennant@ucsc.edu>
2021-05-19 03:05:45 -0700

    node & pq ADT demonstrated

--- asgn6/encode.c
+++ asgn6/encode.c
@@ -4,39 +4,52 @@
 #include <stdio.h>
 
 int main(void) {
-    PriorityQueue *pq = pq_create(8);
-    pq_print(pq);
 
-    enqueue(pq, node_create('a', 323));
-    enqueue(pq, node_create('b', 32));
-    enqueue(pq, node_create('c', 3));
+    PriorityQueue *pq = pq_create(8);
+
+    Node *a = node_create('a', 234);
+    Node *Y = node_create('Y', 7);
+    Node *A = node_create('A', 43);
+    Node *o = node_create('o', 114);
+
+    Node *parent = node_join(o, a);
+
+    enqueue(pq, a);
+    enqueue(pq, Y);
+    enqueue(pq, A);
+    enqueue(pq, o);
+    enqueue(pq, parent);
+
     pq_print(pq);
 
     Node *trash;
     dequeue(pq, &trash);
     dequeue(pq, &trash);
     dequeue(pq, &trash);
+    dequeue(pq, &trash);
+
     pq_print(pq);
 
-    enqueue(pq, node_create('a', 323));
-    enqueue(pq, node_create('b', 32));
-    enqueue(pq, node_create('c', 3));
+    enqueue(pq, Y);
+
     pq_print(pq);
 
-    enqueue(pq, node_create('d', 42));
+    enqueue(pq, a);
+    enqueue(pq, Y);
+    enqueue(pq, A);
+    enqueue(pq, o);
+    enqueue(pq, parent);
+
     pq_print(pq);
 
-    enqueue(pq, node_create('e', 69));
-    pq_print(pq);
-
-    enqueue(pq, node_create('f', 420));
-    pq_print(pq);
-
-    enqueue(pq, node_create('g', 12));
-    pq_print(pq);
-
-    enqueue(pq, node_create('h', 7));
-    pq_print(pq);
+    node_delete(&a);
+    node_delete(&a);
+    node_delete(&Y);
+    node_delete(&A);
+    node_delete(&o);
+    node_delete(&parent);
+    pq_delete(&pq);
+    pq_delete(&pq);
 
     return 0;
 }
