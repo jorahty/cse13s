3d1fa3e8f17c52badb9d7004ef6dabf494a7ea10
James Tennant <jtennant@ucsc.edu>
2021-05-19 12:10:41 -0700

    restored encode.c from old-encode.c

--- asgn6/encode.c
+++ asgn6/encode.c
@@ -1,55 +1,97 @@
+#include "defines.h"
+#include "header.h"
+#include "huffman.h"
+#include "io.h"
 #include "node.h"
-#include "pq.h"
 
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 
-int main(void) {
+#define OPTIONS "hvi:o:"
 
-    PriorityQueue *pq = pq_create(8);
+static void help(char *exec) {
+    // Print help message (inspired by resource program)
+    fprintf(stderr,
+        "SYNOPSIS\n"
+        "  A Huffman encoder.\n"
+        "  Compresses a file using the Huffman coding algorithm.\n"
+        "\n"
+        "USAGE\n"
+        "  %s [-h] [-v] [-i infile] [-o outfile]\n"
+        "\n"
+        "OPTIONS\n"
+        "  -h             Program usage and help.\n"
+        "  -v             Print compression statistics.\n"
+        "  -i infile      Input file to compress.\n"
+        "  -o outfile     Output of compressed data.\n",
+        exec);
+}
 
-    Node *a = node_create('a', 234);
-    Node *Y = node_create('Y', 7);
-    Node *A = node_create('A', 43);
-    Node *o = node_create('o', 114);
+int main(int argc, char **argv) {
+    // Parse command-line arguments
+    FILE *infile = stdin, *outfile = stdout;
+    bool verbose = false;
+    int opt;
+    while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+        switch (opt) {
+        case 'i': infile = fopen(optarg, "r"); break;
+        case 'o': outfile = fopen(optarg, "w"); break;
+        case 'v': verbose = true; break;
+        case 'h':
+            help(argv[0]);
+            fclose(infile);
+            fclose(outfile);
+            return 0;
+        default:
+            help(argv[0]);
+            fclose(infile);
+            fclose(outfile);
+            return 1;
+        }
+    }
+    /*
+    // Create histogram
+    uint64_t hist[ALPHABET] = { 0 }; // This will initialize all values to zero
+    uint8_t buf[BLOCK];
+    while (read_bytes(infile, *buf, BLOCK) != -1) {
+        for (int i; i < BLOCK; i++) {
+            hist[buf[i]]++;
+        }
+    }
+    hist[0]++;
+    hist[255]++;
 
-    Node *parent = node_join(o, a);
+    // Print histogram (temporary)
+    for (int i = 0; i < ALPHABET; i++) {
+        printf("%d %lu\n", i, hist[i]);
+    }
 
-    enqueue(pq, a);
-    enqueue(pq, Y);
-    enqueue(pq, A);
-    enqueue(pq, o);
-    enqueue(pq, parent);
+    // Construct Huffman tree
+    Node *root = build_tree(hist);
 
-    pq_print(pq);
+    // Construct code table
+    Code table[ALPHABET];
+    for (int i = 0; i < ALPHABET; i++) {
+        table[i] = code_init();
+    }
+    build_codes(root, table);
 
-    Node *trash;
-    dequeue(pq, &trash);
-    dequeue(pq, &trash);
-    dequeue(pq, &trash);
-    dequeue(pq, &trash);
+    // Construct header
+    Header *h = malloc(sizeof(Header));
+    h->magic = MAGIC;
+    h->permissions = file permisions;
+    h->tree_size = tree_size;
+    h->file_size = file_size;
 
-    pq_print(pq);
-
-    enqueue(pq, Y);
-
-    pq_print(pq);
-
-    enqueue(pq, a);
-    enqueue(pq, Y);
-    enqueue(pq, A);
-    enqueue(pq, o);
-    enqueue(pq, parent);
-
-    pq_print(pq);
-
-    node_delete(&a);
-    node_delete(&a);
-    node_delete(&Y);
-    node_delete(&A);
-    node_delete(&o);
-    node_delete(&parent);
-    pq_delete(&pq);
-    pq_delete(&pq);
+    // Free memory
+    node_delete(&root);
+    free(h);
+*/
+    // Close infile and outfile
+    fclose(infile);
+    fclose(outfile);
 
     return 0;
 }
