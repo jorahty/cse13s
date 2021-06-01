929517e91250511c3c1fd13949fbc8593a78a9cc
James Tennant <jtennant@ucsc.edu>
2021-05-31 20:23:45 -0700

    node_print is too pretty

--- asgn7/banhammer.c
+++ asgn7/banhammer.c
@@ -4,9 +4,47 @@
 
 int main(void) {
 
-	Node *n = node_create("hello", "clyde");
-	node_print(n);
-	node_delete(&n);
+    Node *a = node_create(NULL, NULL);
+    printf("\n");
+    node_print(a);
+    printf("\n");
+
+    a->oldspeak = "Tyrion";
+    printf("\n");
+    node_print(a);
+    printf("\n");
+
+    a->newspeak = "Aria";
+    printf("\n");
+    node_print(a);
+    printf("\n");
+
+    a->oldspeak = NULL;
+    printf("\n");
+    node_print(a);
+    printf("\n");
+
+    a->next = a;
+    printf("\n");
+    node_print(a);
+    printf("\n");
+
+    a->prev = a;
+    printf("\n");
+    node_print(a);
+    printf("\n");
+
+    a->next = NULL;
+    printf("\n");
+    node_print(a);
+    printf("\n");
+
+    a->oldspeak = "Stark";
+    a->newspeak = "Lannister";
+    a->prev = node_create(NULL, NULL);
+    printf("\n");
+    node_print(a);
+    printf("\n");
 
     return 0;
 }
--- asgn7/node.c
+++ asgn7/node.c
@@ -1,47 +1,155 @@
 #include "node.h"
 
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 Node *node_create(char *oldspeak, char *newspeak) {
-	// Allocate memory for a new node `n`
-	Node *n = (Node *) malloc(sizeof(Node));
+    // Allocate memory for a new node `n`
+    Node *n = (Node *) malloc(sizeof(Node));
 
-	// Allocate memory for the new node's oldspeak and newspeak fields
-	n->oldspeak = (char *) malloc(strlen(oldspeak) + 1); // Add 1 for the null terminator
-	n->newspeak = (char *) malloc(strlen(newspeak) + 1); // Same goes for newspeak
+    // Set n->oldspeak
+    if (oldspeak) {
+        n->oldspeak = (char *) malloc(strlen(oldspeak) + 1);
+        strcpy(n->oldspeak, oldspeak);
+    } else {
+        n->oldspeak = NULL;
+    }
 
-	// Copy the oldspeak and newspeak strings from the given paramters
-	strcpy(n->oldspeak, oldspeak);
-	strcpy(n->newspeak, newspeak);
+    // Set n->newspeak
+    if (newspeak) {
+        n->newspeak = (char *) malloc(strlen(newspeak) + 1);
+        strcpy(n->newspeak, newspeak);
+    } else {
+        n->newspeak = NULL;
+    }
 
-	// Set the `next` and `previous` fields to null for now
-	n->next = NULL;
-	n->prev = NULL;
+    // Set the `next` and `previous` fields to null for now
+    n->next = NULL;
+    n->prev = NULL;
 
-	// Return the new node `n`
-	return n;
+    // Return the new node `n`
+    return n;
 }
 
 void node_delete(Node **n) {
-	// If `n` and `*n` are not already null ...
-	if (n && *n) {
-		// Free memory allocated for oldspeak and newspeak
-		free((*n)->oldspeak);
-		free((*n)->newspeak);
+    if (n && *n) {
+        // Free memory allocated for oldspeak
+        if ((*n)->oldspeak) {
+            free((*n)->oldspeak);
+            (*n)->oldspeak = NULL;
+        }
 
-		// Free the node
-		free(*n);
+        // Free memory allocated for newspeak
+        if ((*n)->newspeak) {
+            free((*n)->newspeak);
+            (*n)->newspeak = NULL;
+        }
 
-		// Set pointer to null 
-		*n = NULL;
-	}
-	return;
+        // Free the node
+        free(*n);
+
+        // Set pointer to null
+        *n = NULL;
+    }
+    return;
+}
+
+static inline int maxlen(char *oldspeak, char *newspeak) {
+    int a = oldspeak ? strlen(oldspeak) + 2 : 4;
+    int b = newspeak ? strlen(newspeak) + 2 : 4;
+    return a >= b ? a : b;
+}
+
+static inline void quote(char *word, char *quoted, int len) {
+    quoted[0] = '"';
+    for (int i = 0; i < len; i += 1) {
+        quoted[i + 1] = word[i];
+    }
+    quoted[len + 1] = '"';
+    quoted[len + 2] = '\0';
+    return;
+}
+
+static inline int ndigits(uint64_t x) {
+    int n = 0;
+    while (x != 0) {
+        x /= 16;
+        n++;
+    }
+    return n;
 }
 
 void node_print(Node *n) {
-	fprintf(stderr, "→ ⎡ \"%s\" ⎤ →\n", n->oldspeak);
-	fprintf(stderr, "← ⎣ \"%s\" ⎦ ←\n", n->newspeak);
-	return;
+    int w = maxlen(n->oldspeak, n->newspeak);
+    int s;
+
+    if (n->prev) {
+        fprintf(stderr,
+            "\x1b[32m"
+            "%p"
+            "\x1b[0m",
+            (void *) n->prev);
+        s = ndigits((uint64_t) n->prev) + 4;
+    } else {
+        fprintf(stderr, "\x1b[31m"
+                        "Null"
+                        "\x1b[0m");
+        s = 6;
+    }
+
+    if (n->oldspeak) {
+        int len_oldspeak = strlen(n->oldspeak);
+        char quoted_oldspeak[len_oldspeak + 3];
+        quote(n->oldspeak, quoted_oldspeak, len_oldspeak);
+        fprintf(stderr,
+            " ← ⎡ "
+            "\x1b[34m"
+            "%-*s"
+            "\x1b[0m"
+            " ⎤ → ",
+            w, quoted_oldspeak);
+    } else {
+        fprintf(stderr, " ← ⎡ "
+                        "\x1b[31m");
+        fprintf(stderr, "%-*s", w, "Null");
+        fprintf(stderr, "\x1b[0m"
+                        " ⎤ → ");
+    }
+
+    if (n->next) {
+        fprintf(stderr,
+            "\x1b[32m"
+            "%p"
+            "\x1b[0m"
+            "\n",
+            (void *) n->next);
+    } else {
+        fprintf(stderr, "\x1b[31m"
+                        "Null"
+                        "\x1b[0m"
+                        "\n");
+    }
+
+    if (n->newspeak) {
+        int len_newspeak = strlen(n->newspeak);
+        char quoted_newspeak[len_newspeak + 3];
+        quote(n->newspeak, quoted_newspeak, len_newspeak);
+        fprintf(stderr,
+            "%*c ⎣ "
+            "\x1b[34m"
+            "%-*s"
+            "\x1b[0m"
+            " ⎦\n",
+            s, ' ', w, quoted_newspeak);
+    } else {
+        fprintf(stderr, "%*c ⎣ ", s, ' ');
+        fprintf(stderr, "\x1b[31m");
+        fprintf(stderr, "%-*s", w, "Null");
+        fprintf(stderr, "\x1b[0m"
+                        " ⎦\n");
+    }
+
+    return;
 }
