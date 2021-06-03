6b7cf8df5217bdc00d730d450daa6d2d0bd24a99
James Tennant <jtennant@ucsc.edu>
2021-06-02 19:19:47 -0700

    can parse opts

--- asgn7/banhammer.c
+++ asgn7/banhammer.c
@@ -3,43 +3,92 @@
 #include "ht.h"
 #include "ll.h"
 #include "node.h"
+#include "parser.h"
 
-#include <stdbool.h>
+#include <getopt.h>
+#include <regex.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
+#define OPTIONS "hsmt:f:"
+#define WORD    "[a-zA-Z0-9_]+(('|-|'-|-')*[a-zA-Z0-9_]+)*"
 
-    HashTable *ht = ht_create(7, true);
+// Inspired by error.c from asgn5
+static void help(char *exec) {
+    // Print help message
+    fprintf(stderr,
+        "SYNOPSIS\n"
+        "  A word filtering program for the GPRSC.\n"
+        "  Filters out and reports bad words parsed from stdin.\n"
+        "\n"
+        "USAGE\n"
+        "  %s [-hsm] [-t size] [-f size]\n"
+        "\n"
+        "OPTIONS\n"
+        "  -h           Program usage and help.\n"
+        "  -s           Print program statistics.\n"
+        "  -m           Enable move-to-front rule.\n"
+        "  -t size      Specify hash table size (default: 10000).\n"
+        "  -f size      Specify Bloom filter size (default: 2^20).\n",
+        exec);
+}
 
-    ht_insert(ht, "Stark", "Lannister");
-    ht_insert(ht, "Arya", "Hound");
-    ht_insert(ht, "Mountain", "King");
-    ht_insert(ht, "Landing", "Winterfell");
-    ht_insert(ht, "Dorne", NULL);
-    ht_insert(ht, "White", "Walker");
-    ht_insert(ht, "Wall", "Night");
-    ht_insert(ht, "Watch", "Red");
-    ht_insert(ht, "Women", "Bastard");
-    ht_insert(ht, "Tyrion", "Jaime");
-    ht_insert(ht, "Cercei", "Sansa");
-    ht_insert(ht, "Tywin", "Shae");
-    ht_insert(ht, "Robb", "Bran");
-    ht_insert(ht, "Queen", "Tyrell");
+int main(int argc, char **argv) {
+    // Parse command-line arguements
+    bool s = false;
+    bool mtf = false;
+    uint32_t t = 10000;
+    uint32_t f = 1 << 20; // 2^20
+    int opt;
+    while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+        switch (opt) {
+        case 's': s = true; break;
+        case 'm': mtf = true; break;
+        case 't': t = atoi(optarg); break;
+        case 'f': f = atoi(optarg); break;
+        case 'h': help(argv[0]); return 0;
+        default: help(argv[0]); return 1;
+        }
+    }
 
-    ht_print(ht);
-    fprintf(stderr, "Size: %d\n", ht_size(ht));
-    fprintf(stderr, "Count: %d\n", ht_count(ht));
+    // Initialize Bloom filter and hash table
+    BloomFilter *bf = bf_create(f);
+    HashTable *ht = ht_create(t, mtf);
 
-    ht_lookup(ht, "Stark");
-    ht_lookup(ht, "Stark");
-    ht_lookup(ht, "Dorne");
-    ht_lookup(ht, "Wall");
+    // Add words from badspeak.txt to bf and ht
 
-    ht_print(ht);
-    fprintf(stderr, "Size: %d\n", ht_size(ht));
-    fprintf(stderr, "Count: %d\n", ht_count(ht));
+    // Add words from newspeak.txt to bf and ht
 
+    // Read data from stdin
+    // For each word,
+    // Query bf
+    // If in bf
+    // Check ht
+    // Else
+    // It's not a bad word
+
+    // Free Bloom filter and Hash Table
+    bf_delete(&bf);
     ht_delete(&ht);
 
     return 0;
 }
+
+// Notes:
+// Compile regex
+// regex_t re;
+// if (regcomp(&re, WORD, REG_EXTENDED)) {
+// 	fprintf(stderr, "Failed to compile regex.\n");
+// 	return 1;
+// }
+
+// FILE *badspeak = fopen("badspeak.txt", "r");
+// FILE *outfile = fopen("output.txt", "w");
+
+// char *word = NULL;
+// while ((word = next_word(badspeak, &re)) != NULL) {
+// 	fprintf(outfile, "%s\n", word);
+// }
+
+// clear_words();
+// regfree(&re);
